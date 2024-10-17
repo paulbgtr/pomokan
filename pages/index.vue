2a10adc07a3b75a47fbc86fa4c02369adf3c3da8
paulbg <paulbg@hey.com>
2024-10-17 15:12:13 +0300

    feat: basic design of the index page

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const columns = ref([
+    {
+        name: 'Todo',
+        tasks: [],
+    },
+])
+</script>
+
+<template>
+    <Header />
+
+    <section class="mt-6 flex-grow flex overflow-x-auto">
+        <Column v-for="column in columns" :key="column.name" :column="column" />
+        <NewColumn />
+    </section>
+</template>
