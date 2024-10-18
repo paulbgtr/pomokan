921ff7b556331df57635848b3bd52fa6a6aabc91
paulbg <paulbg@hey.com>
2024-10-18 11:55:53 +0300

    feat: implement new column functionality

--- components/NewColumn.vue
+++ components/NewColumn.vue
@@ -1,5 +1,29 @@
+<script setup>
+const columnName = ref('')
+const isInputOpen = ref(false)
+
+const emit = defineEmits(['addColumn'])
+
+const addColumn = (name) => {
+    if (name.length > 0) {
+        emit('addColumn', name)
+        columnName.value = ''
+        isInputOpen.value = false
+    }
+}
+</script>
+
 <template>
-    <div>
-        <button class="w-80 btn border-dashed hover:border-primary border-2 border-gray-300">New Column</button>
+    <div class="w-80">
+        <button v-if="!isInputOpen" class="w-full border-2 border-gray-300 border-dashed btn hover:border-primary"
+            @click="isInputOpen = true">
+            + New Column
+        </button>
+        <div v-else class="w-full join">
+            <input type="text" placeholder="Type here"
+                class="flex-grow input input-bordered join-item focus:outline-none" v-model="columnName"
+                @keyup.enter="addColumn(columnName)" />
+            <button class="btn btn-primary join-item" @click="addColumn(columnName)">+</button>
+        </div>
     </div>
 </template>
