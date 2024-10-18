43741c6da41d56ac4d80e781ad967c7346e86e4d
paulbg <paulbg@hey.com>
2024-10-18 11:55:24 +0300

    feat: simple task component

---
+++ components/Task.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    task: {
+        type: String,
+        required: true
+    },
+    isDone: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <div class="p-2 border-2 border-gray-300 rounded-md">
+        <p>{{ task }}</p>
+    </div>
+</template>
