9e240e6b9c41daa96c3d1413f67987942505a426
paulbg <paulbg@hey.com>
2024-10-18 11:56:31 +0300

    chore: extract kanban to its own component

---
+++ components/Kanban.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const columns = ref([
+    {
+        name: 'Todo',
+        tasks: [
+            "buy pepperoni",
+            "buy cheese",
+            "buy bread",
+            "buy butter",
+            "buy milk",
+            "buy eggs",
+            "buy flour",
+            "buy sugar",
+            "buy salt",
+        ],
+    },
+])
+
+const addColumn = (name) => {
+    columns.value.push({
+        name,
+        tasks: [],
+    })
+}
+</script>
+
+<template>
+    <section class="flex flex-grow mt-6 overflow-x-auto">
+        <Column v-for="column in columns" :key="column.name" :column="column" :tasks="column.tasks" />
+        <NewColumn @addColumn="addColumn" />
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,5 @@
-<script setup>
-import { ref } from 'vue'
-
-const columns = ref([
-    {
-        name: 'Todo',
-        tasks: [],
-    },
-])
-</script>
-
 <template>
     <Header />
 
-    <section class="mt-6 flex-grow flex overflow-x-auto">
-        <Column v-for="column in columns" :key="column.name" :column="column" />
-        <NewColumn />
-    </section>
+    <Kanban />
 </template>
