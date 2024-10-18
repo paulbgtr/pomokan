cdad3473d446cfc89e238f523854d063acd11777
paulbg <paulbg@hey.com>
2024-10-18 11:55:33 +0300

    feat: add task prop to column

--- components/Column.vue
+++ components/Column.vue
@@ -3,16 +3,20 @@ defineProps({
     column: {
         type: Object,
         required: true
+    },
+    tasks: {
+        type: Array,
+        required: true
     }
 })
 </script>
 
 <template>
-    <div class="border-2 w-80 flex-shrink-0 flex flex-col rounded-md p-2 border-gray-300 mr-4">
-        <h2 class="text-center text-lg font-bold mb-2">{{ column.name }}</h2>
-        <button class="w-full btn btn-primary mb-2">New Task</button>
-        <div class="flex-grow overflow-y-auto">
-            <!-- Task list will go here -->
+    <div class="flex flex-col flex-shrink-0 p-2 mr-4 border-2 border-gray-300 rounded-md w-80">
+        <h2 class="mb-2 text-lg font-bold text-center">{{ column.name }}</h2>
+        <button class="w-full mb-2 btn btn-primary">New Task</button>
+        <div class="flex-grow space-y-2 overflow-y-auto">
+            <Task v-for="task in tasks" :key="task" :task="task" />
         </div>
     </div>
 </template>
